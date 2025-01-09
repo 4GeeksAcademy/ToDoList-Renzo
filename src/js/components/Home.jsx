49a96dcc720a9e0dcb35315f3897e6cb2b1d93ab
relo-ga <40379438+relo-ga@users.noreply.github.com>
2025-01-09 01:24:57 +0000

    To do list final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,7 +5,24 @@ const Home = () => {
 
 	const [newTask, setNewTask] = useState("");
 	const [pendingTask, setPendingTask] = useState([]);
-	const [hidden, setHidden] = useState(true);
+	
+
+	const Task = (props) =>{
+		const [hidden, setHidden] = useState(true);
+		return (
+			<div className="d-flex align-items-center form-control" style={{borderBottom:"1px solid #e3e3e3", backgroundColor:"white"}}
+						onMouseEnter={()=>{	setHidden(false); console.log(hidden); }}
+						onMouseLeave={()=>{ setHidden(true); console.log(hidden); }}>	
+							<p className="p-3 col-11 fs-3 fw-light text-start" style={{marginBottom:"0"}} key={props.task}>{props.task}</p>
+							{hidden ? null :
+								<i className="col-1 fa-duotone fa-solid fa-xmark " onClick={()=>{
+									setPendingTask([...pendingTask].filter((value)=> value !== props.task))
+									}}>
+								</i>
+							}
+			</div>
+		);
+	}
 
 	return (
 		<div className="text-center">
@@ -22,19 +39,7 @@ const Home = () => {
 					}
 				}}
 				 value={newTask} />
-				{pendingTask.map(task => 
-					<div className="d-flex align-items-center form-control" style={{borderBottom:"1px solid #e3e3e3", backgroundColor:"white"}}
-						onMouseEnter={() =>{ setHidden(false); console.log("mouse enter") }}
-						onMouseLeave={()=>{ setHidden(true); console.log("mouse leave")	}}>	
-							<p className="p-3 col-11 fs-3 fw-light text-start" style={{marginBottom:"0"}} key={task}>{task}</p>
-							{hidden ? null : 
-								<i className="col-1 fa-duotone fa-solid fa-xmark" onClick={()=>{
-									setPendingTask([...pendingTask].filter((value)=> value !== task))
-									}}>
-								</i>
-							}
-					</div>
-					)}
+				{pendingTask.map(task => <Task key={task} task={task} />)}
 					<div className="form-control text-body-tertiary" style={{backgroundColor:"white"}}>
 						<p className="text-start" style={{marginBottom:"0", fontSize:"12px"}}>{(pendingTask.length) == 0 ? "No hay tareas, añadir tareas" : (pendingTask.length)+"item left"}</p>
 					</div>
